b9217bce230bf67ed7776960252fa87bb0e90a2c
mikoo2004-web <mikemarchand2004@gmail.com>
2026-06-05 14:24:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/kaartspelletje/",
+
   server: {
     host: "127.0.0.1",
     port: 9020,
     strictPort: true
   },
+
   preview: {
     host: "127.0.0.1",
     port: 9020,
