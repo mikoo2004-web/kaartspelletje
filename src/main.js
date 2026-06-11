7f3900addec56fd18be48babd3688b3dc5f5356d
mikoo2004-web <mikemarchand2004@gmail.com>
2026-06-11 17:42:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { bindUi, render } from "./board.js?v=bug-keyword-fixes-1";
+import { bindUi, render } from "./board.js?v=stack-area-true-bunker-1";
 import { resetGame } from "./gameState.js";
 import { renderRulesPage } from "./rulesPage.js";
 import { bindTestPanel } from "./testRunner.js";
